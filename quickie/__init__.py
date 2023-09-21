ef3165d90ae92dfc3d7b886912592aa921677686
dennisappiahALX <appiahdennis1998@gmail.com>
2023-09-21 01:42:43 +0000

    db migration

--- quickie/__init__.py
+++ quickie/__init__.py
@@ -1,2 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from quickie.config import Config
+from flask_cors import CORS
 
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    CORS(app, resources={r"*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type, Authorization"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"
+        )
+
+        return response
+
+    return app
---
+++ quickie/models.py
@@ -0,0 +1,78 @@
+from quickie import db
+from sqlalchemy import Column, String, Integer
+
+
+class Question(db.Model):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
+
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "question": self.question,
+            "answer": self.answer,
+            "category": self.category,
+            "difficulty": self.difficulty,
+        }
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
+
+    def __init__(self, type):
+        self.type = type
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def format(self):
+        return {"id": self.id, "type": self.type}
+
+
+class Leaderboard(db.Model):
+    __tablename__ = "leaderboard"
+
+    id = Column(Integer, primary_key=True)
+    player = Column(String)
+    score = Column(Integer)
+
+    def __init__(self, player, score):
+        self.player = player
+        self.score = score
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "player": self.player,
+            "score": self.score,
+        }
